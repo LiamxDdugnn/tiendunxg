c88a224b92c087df66fb1e82f5e7af8f038a718c
LiamxDdugnn <tdungx02@gmail.com>
2025-03-08 22:35:51 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
-const nameGirl = 'Thảo';
+const nameGirl = 'Cháu Ngọc';
 const giftUrl = 'http://nodemy.vn';
 const eventName = 'Chúc Mừng 8-3';
-const titleCard = 'Tặng người ấy';
-const contentCard = 'Chúc mừng ngày 8/3! Chúc bạn luôn xinh đẹp, rạng rỡ và tràn đầy hạnh phúc. Mong rằng mỗi ngày đều mang đến niềm vui, yêu thương và những điều tốt đẹp nhất. Bạn là một bông hoa đặc biệt, luôn tỏa sáng theo cách riêng của mình. Hãy tận hưởng ngày đặc biệt này với thật nhiều nụ cười và yêu thương!';
+const titleCard = 'Tặng cháu';
+const contentCard = 'Chúc mừng ngày 8/3! Chúc cháu luôn xinh đẹp, mạnh mẽ và tràn đầy niềm vui. Hãy luôn tự tin, tỏa sáng và theo đuổi những ước mơ của mình. Chúc cháu một ngày thật ý nghĩa, nhận được nhiều yêu thương và luôn hạnh phúc!';
 
 // phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
 // Cần hỗ trợ hãy liên hệ: 
 // Mr-Nam http://facebook.com/nam.nodemy
 // Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
-const giftImage = 'https://i.imgur.com/m3EgGC3.jpeg';
+const giftImage = 'https://i.imgur.com/Oca4JSU.jpeg';
 const base64 = '';
 const giftImageBase64 = "data:image/png;base64, " + base64;
